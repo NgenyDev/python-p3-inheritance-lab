19b844c0c593f5575ea18760c5a384e8a959a460
Ngeny Gilbert <ngenygkip@gmail.com>
2024-09-14 20:11:07 +0300

    python-p3-inheritance

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,18 @@
+
 #!/usr/bin/env python
 
 from user import User
 
 class Student(User):
+    def _init_(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []  # Initialize 'knowledge' as an empty list
+
+
     
-    def learn(self):
+    
+    
+    def learn(self,knowledge_str):
+        self.knowledge.append(knowledge_str)
         pass
